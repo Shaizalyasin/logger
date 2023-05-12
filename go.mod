c34faf36766f9625432f2e343f1b0510c199cb50
muhammad.shaizal.yasin <shaizalyasin786@gmail.com>
2023-05-12 16:27:45 +0300

    Added logger

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Shaizalyasin/logger
+
+go 1.20
---
+++ logger.go
@@ -0,0 +1,13 @@
+package logger
+
+func Info(data Interface{}){
+fmt.Println("Log info: ", data)
+}
+
+func Error(data Interface{}){
+fmt.Println("Log error: ", data)
+}
+
+func Warning(data Interface){}{
+fmt.Println("Warning info: ", data)
+}
